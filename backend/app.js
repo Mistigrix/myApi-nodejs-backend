57921091110f2d9359cb1f1ffec89a4d25a96965
Gamer Tag <kodanadiarra456@gmail.com>
2023-01-18 17:48:30 +0000

    Recreate myApi

--- backend/app.js
+++ backend/app.js
@@ -1,112 +1,130 @@
-const { request } = require('express');
 const express = require('express');
-const mongoose = require('mongoose');
-const Product = require('./models/product');
+const mysql = require('mysql');
+const myConnection = require('express-myconnection');
+const { application } = require('express');
+
+dbConfig = {
+    host: 'localhost',
+    user: 'root',
+    password: 'rootpassword',
+    port: null,
+    database: 'test',
+}
+
 const app = express();
 
-
-const Connection = require('tedious').Connection;
-const Request = require('tedious').Request;
-
-const config = {
-    server: 'localhost',
-    authentification: {
-        type: 'default',
-        options: {
-            userName: 'root',
-            password: ''
-        }
-    }
-}
-
-const connection = new Connection(config)
-
-connection.on('connect', (err) => {
-    if (err) {
-        conslole.log(err);
-    } else {
-        executeStatement();
-    }
-})
-
-function executeStatement() {
-    request = new Request("select 123", "hello world", (err, rowCount) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log('${rowCount} rows');
-        }
-        connection.close();
-    });
-
-    request.on('row', (columns) => {
-        columns.forEach(column => {
-            if (column.value === null) {
-                console.log('Null');
-            } else {
-                console.log(column.value);
-            }
-        });
-    });
-
-    connection.execSql(request);
-}
-
-
-
-
-
-
-
-
-
-
-mongoose.connect('mongodb+srv://Mistigrix:myapi@cluster0.lgdxmyz.mongodb.net/?retryWrites=true&w=majority', (error) => {
-    if (error) {
-        return console.error(error);
-    }
-    console.log("Connextion réussit");
-});
+app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
-
-app.get('/api/products', (req, res, next) => {
-    Product.find()
-    .then((products) => res.statut(200).json(products))
-    .catch((error) => res.status(400).json(error))
-});
-
-app.post('/api/products', (req, res, next) => {
-    const product = new Product({
-        ...req.body
-    });
-    product.save()
-    .then(product => res.status(201).json({ product }))
-    .catch(errors => res.status(400).json(errors))
-
-});
-
-app.get('/api/products/:id', (req, res, next) => {
-    Product.findOne({id: req.params.id})
-    .then((products) => res.status(200).json(products))
-    .catch(errors => res.status(400).json(error))
-});
-
-app.put('/api/products/:id', (req, res, next) => {
-    Product.updateOne({id: req.params.id}, {...req.body, _id: req.params.id})
-    .then(() => res.status(200).json({ message: "Objet modifié"}))
-    .catch(errors => res.status(400).json({errors}))
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Acces-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
 })
 
-app.delete('/api/products/:id', (req, res, next) => {
-    Product.deleteOne({ _id: req.params.id})
-    .then(() => res.status(200).json({ message: "Objet Supprimé"}))
-    .cactch(error => res.status(400).json({error}))
+app.use(myConnection(mysql, dbConfig, 'pool'));
+
+// this is a test route
+app.get('/api/test', (req, res) => {
+    req.getConnection((error, connection) => {
+        if (error) {
+            console.log(error);
+        } else {
+            connection.query('SELECT * FROM users', [], (error, query) => {
+                if (error) {
+                    console.log(error);
+                } else {
+                    res.status(200).json(query);
+                }
+            })
+        }
+    })
 })
 
-module.exports = app;
+// the route for get all products
+app.get('/api/products', (req, res) => {
+    req.getConnection((error, connection) => {
+        if (error) {
+            console.log(error);
+        } else {
+            connection.query('SELECT * FROM products', [], (error, query) => {
+                if (error) {
+                    res.status(400).json(error);
+                } else {
+                    res.status(200).json(query);
+                }
+            })
+        }
+    })
+});
+
+// the route for get a product
+app.get('/api/products/:id', (req, res) => {
+    req.getConnection((error, connection) => {
+        if (error) {
+            console.log(error);
+        } else {
+            connection.query('SELECT * FROM products WHERE id=' + req.params.id, [], (error, query) => {
+                if (error) {
+                    res.status(404).json(error); // return error status because the product is not found
+                } else {
+                    res.status(200).json({product: query}); // the product has been found, so return success status code and the object
+                }
+            })
+        }
+    })
+});
+
+// the route for add new product in the database
+app.post('/api/products', (req, res) => {
+    req.getConnection((error, connection) => {
+        if (error) {
+            console.log(error);
+        } else {
+            connection.query('INSERT INTO products(name, description, price, inStock) VALUES(?, ?, ?, ?)',[req.body.name, req.body.description, req.body.price, req.body.inStock], (error, query) => {
+                if (error) {
+                    res.status(400).json(error);
+                } else {
+                    res.status(201).json({ query });
+                }
+            })
+        }
+    })
+});
+
+// the route for modified product in the database
+app.put('/api/products/:id', (req, res) => {
+    req.getConnection((error, connection) => {
+        if (error) {
+            console.log(error);
+        } else {
+            connection.query('UPDATE products SET name=?, description=?, price=?, inStock=? WHERE id=?',[req.body.name, req.body.description, req.body.price, req.body.inStock, req.body._id], (error, query) => {
+                if (error) {
+                    res.status(400).json(error);
+                } else {
+                    res.status(201).json({message: 'Modified!' });
+                }
+            })
+        }
+    })
+});
+
+// the route for delete a product in the database
+app.delete('/api/products/:id', (req, res) => {
+    req.getConnection((error, connection) => {
+        if (error) {
+            console.log(error);
+        } else {
+            connection.query('DELETE FROM products WHERE id=?',[req.body._id], (error, query) => {
+                if (error) {
+                    res.status(400).json(error);
+                } else {
+                    res.status(201).json({message: 'Deleted!' });
+                }
+            })
+        }
+    })
+});
+
+PORT = process.env.PORT || 3000;
+app.listen(PORT, console.log("Server started at port: ", PORT));
--- backend/models/user.js
+++
@@ -1,8 +0,0 @@
-const test_sequelize = require('sequelize');
-
-const sequelize = new test_sequelize.Sequelize('sqlite:../database.sqlite');
-
-const User = test_sequelize.define('User', {
-    username: test_sequelize.DataTypes.STRING,
-    birthday: test_sequelize.DataTypes.DATE
-});
